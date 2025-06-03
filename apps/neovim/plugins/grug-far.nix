178c18d3ffe0a47902aef01d197ac5002f61fada
Eleonora <eleonora@tennyson.io>
2025-06-03 09:46:31 -0700

    tidying up (#12)

--- apps/neovim/plugins/grug-far.nix
+++ apps/neovim/plugins/grug-far.nix
@@ -6,15 +6,7 @@
   extraConfigLua = ''
     require('grug-far').setup({})
 
-    vim.keymap.set('n', '<leader>fr', function()
-      require('grug-far').open({ 
-        prefills = { 
-          search = vim.fn.expand("<cword>")
-        } 
-      })
-    end)
-
-    vim.keymap.set('x', '<leader>fr', function()
+    vim.keymap.set({'n', 'x'}, '<leader>fr', function()
       require('grug-far').open({ 
         prefills = { 
           search = vim.fn.expand("<cword>")
